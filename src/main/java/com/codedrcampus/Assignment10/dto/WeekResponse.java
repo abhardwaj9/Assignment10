4f58699b2d2c892b18a9fcf30b9b01a2d757b36a
AKSHAY BHARDWAJ <akshay01_2008@yahoo.com>
2022-10-12 17:33:26 -0400

    Initial commit

---
+++ src/main/java/com/codedrcampus/Assignment10/dto/WeekResponse.java
@@ -0,0 +1,18 @@
+package com.codedrcampus.Assignment10.dto;
+
+public class WeekResponse {
+	private Week week;
+
+	public Week getWeek() {
+		return week;
+	}
+
+	public void setWeek(Week week) {
+		this.week = week;
+	}
+
+	@Override
+	public String toString() {
+		return "WeekResponse [week=" + week + "]";
+	}
+}
